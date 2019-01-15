4d9de7cb926520b1fb6e71f7b409528e411beb2a
Charles Wahome <thewahome@users.noreply.github.com>
2019-01-15 17:55:26 +0300

    using nested components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import Navbar from './Navbar/Navbar'
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>My first React app</h1>
       </div>
     );
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div className="navbar">
+                <div>I am a navbar</div>
+            </div>
+        )
+    }
+}
+
+export default Navbar
