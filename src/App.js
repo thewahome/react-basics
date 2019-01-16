001794618ae57cfd8e03cf55963aa6f0e2a3f8b7
Charles Wahome <thewahome@users.noreply.github.com>
2019-01-16 12:27:20 +0300

    conditional output

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import Ninjas from './Ninjas/Ninjas'
 class App extends Component {
   state = {
     ninjas: [
-      { name: 'Michelangelo', belt: 'Orange', id: 1 },
-      { name: 'Leonardo', belt: 'Blue', id: 2 },
-      { name: 'Raphael', belt: 'Red', id: 3 },
-      { name: 'Donatello', belt: 'Purple', id: 4 }
+      { name: 'Michelangelo', belt: 'Orange', id: 1, age: 20 },
+      { name: 'Leonardo', belt: 'Blue', id: 2, age: 21 },
+      { name: 'Raphael', belt: 'Red', id: 3, age: 23 },
+      { name: 'Donatello', belt: 'Purple', id: 4, age: 22 }
     ]
   }
   render() {
     return (
       <div className="App">
         <Navbar title="React Basics" user="Charles Wahome" />
-        <h1>My first React app</h1>
+        <h1>Ninja Turtles</h1>
         <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
--- src/Ninjas/Ninjas.js
+++ src/Ninjas/Ninjas.js
@@ -2,12 +2,29 @@ import React from 'react'
 
 const Ninjas = (props) => {
     const { ninjas } = props
+    /*     
+        // uses the conditional approach
+
+
+        const ninjaList = ninjas.map(ninja => {
+        if (ninja.age > 20) {
+            return (
+                <div className="ninja" key={ninja.id}>
+                    <div>{ninja.name} - {ninja.belt} - {ninja.age}</div>
+                    <hr />
+                </div>
+            )
+        } else {
+            return null
+        }
+    }) */
     const ninjaList = ninjas.map(ninja => {
-        return (
+        return ninja.age > 20 ? (
             <div className="ninja" key={ninja.id}>
-                <div>{ninja.name} - {ninja.belt}</div>
+                <div>{ninja.name} - {ninja.belt} - {ninja.age}</div>
+                <hr />
             </div>
-        )
+        ) : null
     })
     return (
         <div className="ninja-list">
