2712eb351b22019cb4107806a093d6b2e2ee2155
Charles Wahome <thewahome@users.noreply.github.com>
2019-01-16 11:05:10 +0300

    outputting lists

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Navbar from './Navbar/Navbar'
+import Ninjas from './Ninjas/Ninjas'
+
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Michelangelo', belt: 'Orange', id: 1 },
+      { name: 'Leonardo', belt: 'Blue', id: 2 },
+      { name: 'Raphael', belt: 'Red', id: 3 },
+      { name: 'Donatello', belt: 'Purple', id: 4 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <Navbar title="React Basics" user="Charles Wahome" />
         <h1>My first React app</h1>
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
