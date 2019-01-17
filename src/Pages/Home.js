e581ae73231e1e29472f47742b819346879fc87f
Charles Wahome <thewahome@users.noreply.github.com>
2019-01-17 17:52:40 +0300

    higher order components

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import Rainbow from '../hoc/Rainbow'
 
-export default function Home() {
+const Home = () => {
     return (
         <div className="container">
             <h4 className="center">Home</h4>
@@ -8,3 +9,5 @@ export default function Home() {
         </div>
     )
 }
+
+export default Rainbow(Home)
---
+++ src/hoc/Rainbow.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// export default function Rainbow() {
+
+const Rainbow = (WrappedComponent) => {
+    const colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    const randomColour = colours[Math.floor(Math.random() * 5)]
+    const className = randomColour + '-text'
+
+    return (props)=>{
+        return(
+            <div className={className}>
+                <WrappedComponent {...props}/>
+            </div>
+        )
+    }
+}
+
+export default Rainbow
