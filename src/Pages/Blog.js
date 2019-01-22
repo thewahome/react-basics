f733722c83b440f70cdda703ec7bc7f6d6e0061a
Charles Wahome <thewahome@users.noreply.github.com>
2019-01-22 09:21:09 +0300

    introduce redux reducers

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Pokeball from '../pokeball.png'
+import { connect } from "react-redux";
 
-export default class Contact extends Component {
-    state = {
-        posts: []
-    }
-    componentDidMount() {
-        axios.get('http://jsonplaceholder.typicode.com/posts')
-            .then(res => {
-                console.log(res)
-                this.setState({
-                    posts: res.data.slice(0, 10)
+class Blog extends Component {
+    /*     componentDidMount() {
+            axios.get('http://jsonplaceholder.typicode.com/posts')
+                .then(res => {
+                    this.setState({
+                        posts: res.data.slice(0, 10)
+                    })
                 })
-            })
-    }
+        } */
     render() {
-        const { posts } = this.state
+        const { posts } = this.props
         const postList = posts.length ? (
             posts.map(post => {
                 return (
                     <div className="post card" key={post.id}>
-                        <img src={Pokeball} alt={post.title}/>
+                        <img src={Pokeball} alt={post.title} />
                         <div className="card-content">
                             <Link to={'/post/' + post.id}>
                                 <span className="card-title red-text">
@@ -45,3 +42,9 @@ export default class Contact extends Component {
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+export default connect(mapStateToProps)(Blog)
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,21 @@
 const initState = {
-    posts: []
+    posts: [
+        {
+            id: 1,
+            title:'squirtle laid an egg',
+            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium eligendi illum quaerat in amet optio aliquid dicta iure iusto quam, distinctio pariatur sunt temporibus cumque dignissimos commodi quas suscipit debitis?'
+        },
+        {
+            id: 2,
+            title:'charmander laid an egg',
+            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium eligendi illum quaerat in amet optio aliquid dicta iure iusto quam, distinctio pariatur sunt temporibus cumque dignissimos commodi quas suscipit debitis?'
+        },
+        {
+            id: 3,
+            title:'a fossil was found',
+            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium eligendi illum quaerat in amet optio aliquid dicta iure iusto quam, distinctio pariatur sunt temporibus cumque dignissimos commodi quas suscipit debitis?'
+        }
+    ]
 }
 const rootReducer = (state = initState, action) => {
     return state;
