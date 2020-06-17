d85aa142a8a70d9dd901ebcec762517be27340b6
Charles Wahome <thewahome.cw@gmail.com>
2020-06-17 16:04:28 +0300

    use the components in the form

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,59 @@
-import React from 'react'
-import Rainbow from '../hoc/Rainbow'
+import React, { useState } from 'react';
+import Rainbow from '../hoc/Rainbow';
+import { FormInput, SubmitButton } from '../components/form-elements/index';
 
 const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, corrupti ipsam natus quis fugit sed nisi amet. Placeat voluptas quam magnam quisquam, consequatur esse doloribus ipsa, error ipsam cumque aperiam!</p>
-        </div>
-    )
-}
 
-export default Rainbow(Home)
+  const [loading, setLoading] = useState(false);
+
+  const formSubmitted = () => {
+    setLoading(true);
+    setTimeout(() => {
+      alert('Form has been submitted');
+      setLoading(false);
+    }, 3000);
+  };
+  
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
+        corrupti ipsam natus quis fugit sed nisi amet. Placeat voluptas quam
+        magnam quisquam, consequatur esse doloribus ipsa, error ipsam cumque
+        aperiam!
+      </p>
+      <div class="span3">
+        <h2>Sign Up </h2>
+        <div>
+          <FormInput
+            label={'First name'}
+            type={'text'}
+            placeholder={'Enter your first name'}
+          />
+          <FormInput
+            label={'Last name'}
+            type={'text'}
+            placeholder={'Enter your last name'}
+          />
+          <FormInput
+            label={'Email Address'}
+            type={'text'}
+            placeholder={'Enter a valid email'}
+          />
+
+          <SubmitButton
+            title={'Sign Up here'}
+            styling={'btn btn-success'}
+            loading={loading}
+            buttonHasBeenClicked={() => formSubmitted()}
+          />
+
+          <div class="clearfix" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Rainbow(Home);
